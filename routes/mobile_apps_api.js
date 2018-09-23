b84215540630dacab22051183f8268d8b901336b
Rohit <moza.rohit@gmail.com>
2018-09-22 22:38:11 -0400

    Bump setup and some refactoring + testing with postman. Back end routes are complete with accompanying logic

--- routes/mobile_apps_api.js
+++ routes/mobile_apps_api.js
@@ -3,65 +3,88 @@ const router = express.Router();
 
 const AppBuild = require('../models/app-builds');
 
-sendAppExistsResponse = (res) => res.json({success: 200, msg: "Already exists updating"});
-sendAppFoundResponse = (res, AppBuild) => res.json({success: 200, app_build: AppBuild})
-sendAppNotFoundResponse = (res) => res.json({success: 400, msg: 'build id not found'})
+sendBuildCreatedResponse = (res, newAppBuild) => res.json({success: 200, msg: "New AppBuild Created", app_build: newAppBuild});
+sendBuildUpdatedResponse = (res, updatedAppBuildNumber) => res.json({success: 200, msg: "AppBuild Updated", build_number:  updatedAppBuildNumber});
+sendUpdateCreateFailedResponse = (res, err) => res.json({success: 400, err: err});
+sendUpdateAppBuildFailedResponse = (res) => res.json({success: 400, err: "New Build number < Exisiting Build number. Update Failed"});
+sendAppFoundResponse = (res, appBuildNumber) => res.json({success: 200, build_number: appBuildNumber});
+sendAppNotFoundResponse = (res) => res.json({success: 400, err: "Build id not found"});
 
-constructAppBuildObject = (bundle_id) => new AppBuild({ bundle_id: bundle_id, build_number:0})
+constructAppBuildObject = (bundle_id, buildNumber=0) => new AppBuild({ bundle_id: bundle_id, build_number: buildNumber})
 
 addAppBuild = (newAppBuild, res) => {
-  AppBuild.addAppBuild(newAppBuild, (err, AppBuild) => {
+  AppBuild.addAppBuild(newAppBuild, (err, newAppBuild) => {
     if(err) {
-      res.json({success: 400, err: err});
+      sendUpdateCreateFailedResponse(res, err);
     } else {
-      res.json({success: 200, msg: 'New AppBuild Created', app_build: AppBuild });
+      sendBuildCreatedResponse(res, newAppBuild)
     }
   });
 }
 
-updateAppBuild = (AppBuild) => {
+newAppBuildCreate = (reqBundleId, res) => {
+  let newAppBuild = constructAppBuildObject(reqBundleId)
+  addAppBuild(newAppBuild, res);
+}
+
+updateAppBuild = (existingAppBuild, newBuildNumber, res) => {
+
+  AppBuild.updateAppBuildNumber(existingAppBuild, newBuildNumber, (err, updatedAppBuild) => {
+    if(err) {
+      sendUpdateCreateFailedResponse(res, err);
+    } else {
+      sendBuildUpdatedResponse(res, updatedAppBuild.build_number);
+    }
+  });
 
 }
 
 
-router.get('/read', (req, res, next) => {
-    // Example: ./api/read?bundle_id=com.sagomini.HomeworkChallenge
-  let bundleId = req.query.bundle_id;
-  // console.log(req.query.bundle_id);
-  AppBuild.getAppBuildByBundleId(bundleId, (err, AppBuild) => {
+router.get('/read', (req, res) => {
 
-    if (AppBuild) {
-      sendAppFoundResponse(res, AppBuild)
+  let bundleId = req.query.bundle_id;
+
+  AppBuild.getAppBuildByBundleId(bundleId, (err, foundAppBuild) => {
+    if (foundAppBuild) {
+      sendAppFoundResponse(res, foundAppBuild.build_number);
     } else {
       sendAppNotFoundResponse(res);
     }
-     
   });
+
 });
 
-router.post('/set', (req, res, next) => {
+router.post('/set', (req, res) => {
 
   let reqBundleId = req.body.bundle_id;
-  let buildNumber = req.body.build_number;
-  AppBuild.getAppBuildByBundleId(reqBundleId, (err, appBuildFound) => {
-    if (!appBuildFound) {
-      let newAppBuild = constructAppBuildObject(reqBundleId)
-      addAppBuild(newAppBuild, res);
+  let newBuildNumber = req.body.build_number;
+
+  AppBuild.getAppBuildByBundleId(reqBundleId, (err, AppBuildFound) => {
+    if (!AppBuildFound) {
+      newAppBuildCreate(reqBundleId, res);
     } else {
-      sendAppExistsResponse(res);
+      if (newBuildNumber > AppBuildFound.build_number){
+        updateAppBuild(AppBuildFound, newBuildNumber, res);
+      } else {
+        sendUpdateAppBuildFailedResponse(res);
+      }
     }
   })
-  // if bundle_id does not exist.●create and init build_number = 0;■
 
-//   params: ■bundle_id(string)■ new_build_number(int)○ biz logic■
-// if bundle_id does not exist.●create and init build_number = 0;■
-// If new_build_number > existing_build_number● existing_build_number = new_build_number○
-// return :■success: 200■
-// else :4 xx
 });
 
-router.post('/bump', (req, res, next) => {
-  res.json('BUMP');
+router.post('/bump', (req, res) => {
+  let reqBundleId = req.body.bundle_id;
+
+   AppBuild.getAppBuildByBundleId(reqBundleId, (err, AppBuildFound) => {
+    if (!AppBuildFound) {
+      newAppBuildCreate(reqBundleId, res);
+    } else {
+      let newBuildNumber = AppBuildFound.build_number + 1;
+      updateAppBuild(AppBuildFound, newBuildNumber, res);
+    }
+  })
+
 });
 
 module.exports = router;
